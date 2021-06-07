d258718306cf783e574e54c036629ddfbaa4313b
KirillTK <kirill99.tkachev@yandex.by>
2021-06-08 00:38:38 +0300

    Refactor procfile

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,10 +9,14 @@ import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { VehiclesModule } from './modules/vehicles/vehicles.module';
-import { BillsModule } from './modules/bills/bills.module'; 
+import { BillsModule } from './modules/bills/bills.module';
+
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..', 'front', 'build'),
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     UsersModule,
